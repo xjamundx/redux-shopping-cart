63dd0dc04ce5a1217fa18ecd3c3220b78bda7e0d
Jamund Ferguson <jamund@gmail.com>
2021-05-25 12:05:49 -0700

    Revert "removed example reducer"
    This reverts commit d5b785a043423bb2725e2e6ac604630072a11422.

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
 import cartReducer from "./features/cart/cartSlice";
 import productsReducer from "./features/products/productsSlice";
 
@@ -6,5 +7,6 @@ export const store = configureStore({
   reducer: {
     cart: cartReducer,
     products: productsReducer,
+    counter: counterReducer,
   },
 });
