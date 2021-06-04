852bbeb189fc3f1f70ce1e7fbc17f682da424777
Jamund Ferguson <jamund@gmail.com>
2021-06-04 16:48:24 -0700

    finished lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,25 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/products">Products</a>
+          <a href="/cart">Cart</a>
+        </nav>
       </header>
+      <main className="page">
+        <h1>Welcome to the Store</h1>
+        <figure>
+          <img src="https://github.com/xjamundx/redux-shopping-cart/blob/main/public/store.jpg?raw=true"
+               alt="A large old storefront that says 1889 the big store"
+               width="800"/>
+          <figcaption>Gary Houston, CC0, via Wikimedia Commons</figcaption>
+        </figure>
+      </main>
     </div>
   )
 }
