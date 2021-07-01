3c7b52cdd809a197f7d2c9a6954e98662d1a50cf
Jamund Ferguson <jamund@gmail.com>
2021-06-30 17:45:28 -0700

    readme

---
+++ README.md
@@ -0,0 +1,19 @@
+## Redux Shopping Cart
+
+Example shopping cart application for learning redux and redux toolkit. Goes along with the egghead course.
+
+## Setup
+
+Checkout the code base and then type:
+
+```
+npm install
+npm run dev
+```
+
+## Technologies
+
+- [Vite](https://vitejs.dev/)
+- [React Router](https://reactrouter.com/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [CSS Modules](https://github.com/css-modules/css-modules)
--- src/app/api.ts
+++ src/app/api.ts
@@ -13,3 +13,24 @@ export async function getProducts(): Promise<Product[]> {
   const products = results.json();
   return products;
 }
+
+export type CartItems = { [productID: string]: number };
+export type CheckoutResponse = { success: boolean; error?: string };
+
+export async function checkout(items: CartItems): Promise<CheckoutResponse> {
+  const url = "/checkout-error.json";
+  await sleep(500);
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(items),
+  });
+  const data = await response.json();
+  if (!data.success) {
+    throw new Error(data.error);
+  }
+  return data as CheckoutResponse;
+}
+
+// utility function to simulate slowness in an API call
+const sleep = (time: number) =>
+  new Promise((res) => setTimeout(res, time));
