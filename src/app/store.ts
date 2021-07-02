3de1b26fbbabe7458e0b2dd5ae33b785c5c34943
Jamund Ferguson <jamund@gmail.com>
2021-07-02 13:45:39 -0700

    added slices

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productsReducer from "../features/products/productsSlice";
+import cartReducer from "../features/cart/cartSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    products: productsReducer,
+    cart: cartReducer,
+  },
 });
