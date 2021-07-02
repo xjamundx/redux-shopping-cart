44974b100b8b6907f912bc75204da6aa79217590
Jamund Ferguson <jamund@gmail.com>
2021-07-02 16:59:41 -0700

    added real data to shopping cart page

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useAppSelector } from "../../app/hooks";
 import styles from "./Cart.module.css";
 
 export function Cart() {
+  const products = useAppSelector((state) => state.products.products);
+  const items = useAppSelector((state) => state.cart.items);
   return (
     <main className="page">
       <h1>Shopping Cart</h1>
@@ -15,30 +18,26 @@ export function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Magnifying Glass</td>
-            <td>
-              <input type="text" className={styles.input} defaultValue={21} />
-            </td>
-            <td>$44.44</td>
-            <td>
-              <button aria-label="Remove Magnifying Glass from Shopping Cart">
-                X
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>Football Cleats</td>
-            <td>
-              <input type="text" className={styles.input} defaultValue={17} />
-            </td>
-            <td>$25.99</td>
-            <td>
-              <button aria-label="Remove Football Cleats from Shopping Cart">
-                X
-              </button>
-            </td>
-          </tr>
+          {Object.entries(items).map(([id, quantity]) => (
+            <tr>
+              <td>{products[id].name}</td>
+              <td>
+                <input
+                  type="text"
+                  className={styles.input}
+                  defaultValue={quantity}
+                />
+              </td>
+              <td>${products[id].price}</td>
+              <td>
+                <button
+                  aria-label={`Remove ${products[id].name}} from Shopping Cart`}
+                >
+                  X
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
         <tfoot>
           <tr>
