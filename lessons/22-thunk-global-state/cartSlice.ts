3fc749e949c18496e42e6e318b52dc46e94936b9
Jamund Ferguson <jamund@gmail.com>
2021-09-21 21:50:03 -0700

    restored missing arg in lesson 22

--- lessons/22-thunk-global-state/cartSlice.ts
+++ lessons/22-thunk-global-state/cartSlice.ts
@@ -20,7 +20,7 @@ const initialState: CartState = {
   errorMessage: "",
 };
 
-export const checkoutCart = createAsyncThunk("cart/checkout", async () => {
+export const checkoutCart = createAsyncThunk("cart/checkout", async (_, thunkAPI) => {
   const state = thunkAPI.getState() as RootState;
   const items = state.cart.items;
   const response = await checkout(items);
